c7ffa15669689e7716349329d37cf307a5e4bb3a
Abar <abar@protonmail.ch>
2019-01-23 00:11:33 +0200

    Rendering circle, draft for list

---
+++ src/Geometry.hs
@@ -0,0 +1,55 @@
+module Geometry where 
+
+    import Codec.Picture
+
+    data Point = Point { x :: Float
+                        , y :: Float
+                        , z :: Float 
+                        } deriving (Show, Eq, Ord)
+
+    data Circle = Circle { r :: Float
+                         , p :: Point
+                         , c :: PixelRGB8
+                         } deriving Show
+                 
+    type World = [Circle]
+
+    padd  :: Point -> Point -> Point
+    (Point i j k) `padd` (Point l m n) = Point (i+l) (j+m) (k+n)  
+    
+    psub  :: Point -> Point -> Point
+    p1 `psub` p2 = p1 `padd` (p2 `pmul` (-1))
+          
+    pmul :: Point -> Float -> Point  
+    (Point i j k) `pmul` m = Point (i*m) (j*m) (k*m)  
+      
+    pdot :: Point -> Point -> Float
+    (Point i j k) `pdot` (Point l m n) = i*l + j*m + k*n  
+
+    normalize :: Point -> Point 
+    normalize (Point x y z) = Point { x = x / l, y = y / l, z = z / l } 
+        where l = sqrt ( x ** 2 + y ** 2 + z ** 2)
+
+    world :: World
+    world = [
+        Circle {r = 5, p = Point { x = -1, y = -1, z = -10 }, c = PixelRGB8 128 20 20},
+        Circle {r = 7, p = Point { x = 5, y = 5, z = -17 }, c = PixelRGB8 20 128 20},
+        Circle {r = 3, p = Point { x = 1, y = 1, z = -9 }, c = PixelRGB8 20 20 128}
+        ]
+
+    pointCompute :: Circle -> Point -> Point -> Float -> Float -> Float -> Maybe Float
+    pointCompute c o d tca d2 r2 = if t0 < 0 && t1 < 0 then Nothing else Just ret
+        where thc = sqrt(r2 - d2)
+              t0 = tca - thc
+              t1 = tca + thc
+              ret = if t0 < 0 then t1 else t0
+
+    rayIntersect :: Circle -> Point -> Point -> Maybe Float
+    rayIntersect c o d = 
+        if d2 > r2 then 
+            Nothing else pointCompute c o d tca d2 r2
+
+        where l = (p c) `psub` o
+              tca = l `pdot` d
+              d2 = l `pdot` l - tca * tca
+              r2 = (r c) * (r c)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,28 +1,37 @@
 module Lib
     ( someFunc
     ) where
-import Prelude as P
-import Codec.Picture
 
-data Point = Float Float Float
-data Circle = Point Float
-data World = Circle
+    import Prelude as P
+    import Codec.Picture
+    import Data.Tuple
+    import Geometry 
 
-isInside :: Int -> Int -> Bool
-isInside x y = if 
-        x >= 125 
-        && x <= 175 
-        && y >= 125 
-        && y <= 175 then True 
-                    else False
+    data Camera = Camera { fov :: Float, o :: Point }
 
-pixelRenderer :: Int -> Int -> PixelRGB8
-pixelRenderer x y = 
-    if isInside x y then PixelRGB8 128 (fromIntegral x) (fromIntegral y)
-                    else PixelRGB8 0 0 0
+    cam = Camera { fov = 90, o = Point{x = 0, y = 0, z = 0} }
+    back = PixelRGB8 20 50 50
 
-imageCreator :: String -> IO ()
-imageCreator path = writePng path $ generateImage pixelRenderer 300 300
+    pcol :: (PixelRGB8, Maybe Float) -> PixelRGB8
+    pcol (_, Nothing) = back
+    pcol (circle, _) =  circle
 
-someFunc :: IO ()
-someFunc = imageCreator "test.png"
+    calc :: Point -> Point -> Circle -> (PixelRGB8, Maybe Float)
+    calc p1 p2 ci = (c ci, rayIntersect ci p1 p2)
+
+    nearest :: World -> Point -> Point -> PixelRGB8
+    nearest w p1 p2 = pcol (head wt)
+        where wt = map (calc p1 p2) w
+
+    pixelRenderer :: World -> Camera -> Int -> Int -> PixelRGB8
+    pixelRenderer w cam i j = nearest w (o cam) d
+        where rx = (2 * (fromIntegral i + 0.5) / 300 - 1) * tan( (fov cam) / 2.0 ) * 300 / 300
+              ry = -(2 * (fromIntegral j + 0.5) / 300.0 - 1) * tan( (fov cam) / 2.0 )
+              d = normalize Point { x = rx, y = ry, z = -1}
+              c = head w
+
+    imageCreator :: String -> World -> Camera -> IO ()
+    imageCreator path w c = writePng path $ generateImage (pixelRenderer w c) 300 300
+
+    someFunc :: IO ()
+    someFunc = imageCreator "test.png" world cam
